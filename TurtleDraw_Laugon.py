6d8c618dd28ba59faf002555b1739443a59294bb
Laugonr <yourruben2@gmail.com>
2025-04-16 10:30:52 -0500

    Initial commit

---
+++ TurtleDraw_Laugon.py
@@ -0,0 +1,12 @@
+print('TurtleDraw Starting...')
+
+
+tddata = open("turtledraw.txt", "r")
+
+line = tddata.readline()
+
+while line:
+    print(line, end='')
+    parts = line.split(' ')
+    print (parts)
+    line = tddata.readline()
